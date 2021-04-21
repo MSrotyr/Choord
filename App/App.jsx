b5d1ac4cf5226b9ca1e07a3f170e232f69aa9f49
Matt Srotyr <srotyr@gmx.com>
2021-04-21 18:42:32 +0100

    add navigation

--- App/App.jsx
+++ App/App.jsx
@@ -1,20 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Library from './screens/Library';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>This App works!</Text>
-
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Library" component={Library} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
+};
+
---
+++ App/screens/Library.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Library() {
+  return (<View>
+    <Text>Hello I think it's working</Text>
+  </View>)
+}
