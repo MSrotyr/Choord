71e7a4af06c30bf7d90753d6f95c7d1386eb75a9
Matt Srotyr <srotyr@gmx.com>
2024-06-22 13:55:22 +0100

    Removed 3000 Port Default

--- Server/models/index.js
+++ Server/models/index.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const URL = process.env.MONGO_URL || "mongodb://localhost:27017/choord";
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useFindAndModify: false,
